9115d166b300bcc211c857ff19fe918f9ea630d0
vshishkov <vshishkov@amberdata.ru>
2019-12-24 23:31:06 +0300

    initial

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject emptyone/persistents "0.1.0-SNAPSHOT"
+  :description "Clojure persistents utils for working with files."
+  :url "https://github.com/evilsneer/clj-persistents"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :plugins [[org.apache.maven.wagon/wagon-ssh-external "2.6"]]
+  :deploy-repositories ^:replace [["releases" {:url           "scp://root@10.20.30.111/root/rel/"
+                                               :sign-releases false}]
+                                  ["snapshots" "scp://root@10.20.30.111/root/snap/"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :repl-options {:init-ns persistents.core})
+
+
+(cemerick.pomegranate.aether/register-wagon-factory!
+  "scp" #(clojure.lang.Reflector/invokeConstructor
+           (resolve 'org.apache.maven.wagon.providers.ssh.external.ScpExternalWagon)
+           (into-array [])))
---
+++ src/persistents/core.clj
@@ -0,0 +1,30 @@
+(ns persistents.core
+  (:require [clojure.java.io :as io]))
+
+(def exists (memfn exists))
+
+(defn <-disk [name]
+  (if (exists (io/file name))
+    (-> name
+      slurp
+      read-string)))
+
+(defn ->disk [name item]
+  (locking name
+    (->> item
+     str
+     (spit name))))
+
+(defn hdd-synced-atom
+  ([filename]
+   (hdd-synced-atom filename (<-disk filename)))
+  ([filename initial]
+   (hdd-synced-atom filename initial true))
+  ([filename initial replace?]
+   (let [-a (atom (if replace?
+                    initial
+                    (or (<-disk filename) initial)))]
+     (->disk filename initial)
+     (add-watch -a (keyword (str filename "-atom-watcher"))
+       (fn [key atom old-state new-state]
+         (->disk filename new-state))))))
