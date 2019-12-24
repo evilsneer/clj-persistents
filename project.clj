4f9e8cf0c1f31b38e85442124875b0492ff434f4
vshishkov <vshishkov@amberdata.ru>
2019-12-24 23:32:49 +0300

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject emptyone/persistents "0.1.0"
+(defproject emptyone/persistents "0.1.1-SNAPSHOT"
   :description "Clojure persistents utils for working with files."
   :url "https://github.com/evilsneer/clj-persistents"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
