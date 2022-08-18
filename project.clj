644b064490b040b2bc436fc7f043c0f30c4d2c4e
vshishkov <vshishkov@amberdata.ru>
2022-08-18 12:50:02 +0300

    Version 0.1.5

--- project.clj
+++ project.clj
@@ -1,12 +1,20 @@
-(defproject emptyone/persistents "0.1.5-SNAPSHOT"
+(defproject emptyone/persistents "0.1.5"
   :description "Clojure persistents utils for working with files."
   :url "https://github.com/evilsneer/clj-persistents"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :plugins [[org.apache.maven.wagon/wagon-ssh-external "2.6"]]
-  :deploy-repositories ^:replace [["releases" {:url           "scp://root@10.20.30.111/root/rel/"
-                                               :sign-releases false}]
-                                  ["snapshots" "scp://root@10.20.30.111/root/snap/"]]
+  ;:deploy-repositories ^:replace [["releases" {:url           "scp://root@10.20.30.111/root/rel/"
+  ;                                             :sign-releases false}]
+  ;                                ["snapshots" "scp://root@10.20.30.111/root/snap/"]]
+  :release-tasks ^:replace [["vcs" "assert-committed"]
+                            ["change" "version" "leiningen.release/bump-version" "release"]
+                            ["vcs" "commit"]
+                            ["vcs" "tag" "--no-sign"]
+                            ["deploy" "nexus-m9"]
+                            ["change" "version" "leiningen.release/bump-version"]
+                            ["vcs" "commit"]
+                            ["vcs" "push"]]
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns persistents.core})
 
